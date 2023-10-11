cd3053649e446cfa513ab98d444e490383244933
Cristhian Rodrigues <cristhian.rodrigues95@gmail.com>
2023-10-11 20:37:20 -0300

    Minhas modificacoes

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,47 +1,70 @@
-const pokemonList = document.getElementById('pokemonList')
+const pokemonList    = document.getElementById('pokemonList')
 const loadMoreButton = document.getElementById('loadMoreButton')
+let offset         = 0
+const limit        = 5
+const maxRecords   = 779
 
-const maxRecords = 151
-const limit = 10
-let offset = 0;
 
-function convertPokemonToLi(pokemon) {
-    return `
-        <li class="pokemon ${pokemon.type}">
-            <span class="number">#${pokemon.number}</span>
-            <span class="name">${pokemon.name}</span>
 
-            <div class="detail">
-                <ol class="types">
-                    ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join('')}
-                </ol>
-
-                <img src="${pokemon.photo}"
-                     alt="${pokemon.name}">
-            </div>
-        </li>
-    `
-}
-
-function loadPokemonItens(offset, limit) {
-    pokeApi.getPokemons(offset, limit).then((pokemons = []) => {
-        const newHtml = pokemons.map(convertPokemonToLi).join('')
-        pokemonList.innerHTML += newHtml
+function loadPokemonItems(offset, limit){
+    pokeapi.getPokemons(offset, limit).then((pokemons = [])  => {
+    const newHtml = pokemons.map((pokemon) =>
+    ` <li class="pokemon ${pokemon.type}">
+                    <span class="number">#${pokemon.number}</span>
+                    <span class="name">${pokemon.name}</span>
+    
+                    <div class="detail">
+                        <ol class="types">
+                        <li class="type ttitles">Types</li>
+                            ${pokemon.types.map((type) =>`<li class="type ${type}">${type}</li>`).join('')}
+                        </ol>
+                        
+                        <img src="${pokemon.photo}"
+                            alt="${pokemon.name}">
+                    </div>
+                    <div class="detail2">
+                    <ul class="abilities">
+                    <li class="ability ttitles">Abilities</li>
+                    ${pokemon.abilities.map((ability) =>`<li class="ability ${pokemon.type}">${ability}</li>`).join('')}
+                    </ul>
+                    <ul class="sizes">
+                    <li class="size ttitles">Size</li>
+                    <li class="size">Weight: ${pokemon.weight} kg</li>
+                    <li class="size">Height: ${pokemon.height} m</li>
+                    </ul>
+                </div>
+                </li>`
+    ).join('')
+    pokemonList.innerHTML +=  newHtml
+    
     })
+
 }
 
-loadPokemonItens(offset, limit)
+loadPokemonItems(offset, limit)
+
+
 
 loadMoreButton.addEventListener('click', () => {
     offset += limit
-    const qtdRecordsWithNexPage = offset + limit
+    const qtRecords = offset + limit
 
-    if (qtdRecordsWithNexPage >= maxRecords) {
+    if (qtRecords >= maxRecords){
         const newLimit = maxRecords - offset
-        loadPokemonItens(offset, newLimit)
+        loadPokemonItems(offset, newLimit)
 
         loadMoreButton.parentElement.removeChild(loadMoreButton)
-    } else {
-        loadPokemonItens(offset, limit)
+    } else{
+
+        loadPokemonItems(offset, limit)
+
     }
+    
+    
 })
+
+
+
+
+   
+   
--- assets/js/poke-api.js
+++ assets/js/poke-api.js
@@ -1,35 +1,37 @@
-
-const pokeApi = {}
+const pokeapi = {}
 
 function convertPokeApiDetailToPokemon(pokeDetail) {
-    const pokemon = new Pokemon()
+    const pokemon  = new Pokemon()
     pokemon.number = pokeDetail.id
-    pokemon.name = pokeDetail.name
+    pokemon.name   = pokeDetail.name
+    pokemon.height = pokeDetail.height/10
+    pokemon.weight = pokeDetail.weight/10
 
-    const types = pokeDetail.types.map((typeSlot) => typeSlot.type.name)
-    const [type] = types
-
-    pokemon.types = types
-    pokemon.type = type
-
-    pokemon.photo = pokeDetail.sprites.other.dream_world.front_default
-
+    const types        = pokeDetail.types.map((typeSlot) => typeSlot.type.name)
+    const abilities    = pokeDetail.abilities.map((abilitieSlot) => abilitieSlot.ability.name)
+    const [type]       = types
+    pokemon.types      = types
+    pokemon.type       = type
+    pokemon.abilities  = abilities
+    pokemon.photo      = pokeDetail.sprites.other.dream_world.front_default
     return pokemon
 }
 
-pokeApi.getPokemonDetail = (pokemon) => {
+pokeapi.getPokemonDetail = (pokemon) => {
     return fetch(pokemon.url)
-        .then((response) => response.json())
-        .then(convertPokeApiDetailToPokemon)
+           .then((response) => response.json())
+           .then(convertPokeApiDetailToPokemon) 
 }
 
-pokeApi.getPokemons = (offset = 0, limit = 5) => {
-    const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
-
+pokeapi.getPokemons = (offset = 0, limit = 5) => {
+    const url    = `http://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
     return fetch(url)
-        .then((response) => response.json())
-        .then((jsonBody) => jsonBody.results)
-        .then((pokemons) => pokemons.map(pokeApi.getPokemonDetail))
-        .then((detailRequests) => Promise.all(detailRequests))
-        .then((pokemonsDetails) => pokemonsDetails)
+           .then((response)  => response.json())
+           .then((jsonBody)  => jsonBody.results)
+           .then((pokemons)  => pokemons.map(pokeapi.getPokemonDetail))
+           .then((detailRequests) =>Promise.all(detailRequests))
+           .then((pokemonsDetails) =>(pokemonsDetails))
+
+           .catch((error)    => console.error(error))
 }
+
